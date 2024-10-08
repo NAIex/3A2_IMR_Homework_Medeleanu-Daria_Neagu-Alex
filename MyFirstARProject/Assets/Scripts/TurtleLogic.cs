f8e98891b614dfc7fc2cf35d4464e3a4db23f62a
NAIex <alexneagubiz@gmail.com>
2024-10-08 22:09:37 +0300

    Resting Turtles
    The turtles now rest if they are left alone, and you can wake them up!

--- MyFirstARProject/Assets/Scripts/TurtleLogic.cs
+++ MyFirstARProject/Assets/Scripts/TurtleLogic.cs
@@ -6,32 +6,94 @@ using UnityEngine;
 public class TurtleLogic : MonoBehaviour
 {
     private Animator animator;
-    // Start is called before the first frame update
+
+    private Vector3 prevPos;
+    private GameObject opponent;
+
+    private float restDelay = 5.0f;
+    private float nextRestTime;
+    private bool resting;
     void Start()
     {
         this.animator = this.transform.parent.gameObject.GetComponentInChildren<Animator>();
         Debug.Log(this.animator != null);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        Vector3 currentPos = gameObject.transform.position;
+        float dist = Vector3.Distance(prevPos, currentPos);
+
+        bool runDist = Vector3.Distance(prevPos, currentPos) > 0.5;
+        bool walkDist = Vector3.Distance(prevPos, currentPos) > 0.1;
+
+        if (runDist)
+        {
+            animator.SetBool("Run", true);
+            this.WakeUp();
+        }
+        else if(walkDist)
+        {
+            animator.SetBool("Walk", true);
+            this.WakeUp();
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Walk", false);
+        }
+        prevPos = currentPos;
+
+        if (this.opponent != null)
+        {
+            if (resting)
+                this.WakeUp();
+            nextRestTime = Time.time + restDelay;
+
+            this.FaceOpponent();
+        }
         
+        if (!resting && Time.time > nextRestTime)
+        {
+            resting = true;
+            this.Rest();
+        }
+
+    }
+    private void Rest()
+    {
+        this.animator.SetBool("Rest", true);
+    }
+    private void WakeUp()
+    {
+        resting = false;
+        nextRestTime = Time.time + restDelay;
+
+        this.animator.SetBool("Rest", false);
     }
 
-    private void LetsFight()
+    private void FaceOpponent()
+    {
+        this.transform.parent.LookAt(opponent.transform);
+    }
+    private void LetsFight(GameObject opponent)
     {
         this.animator.SetBool("Attack", true);
+        this.opponent = opponent;
     }
     private void GotAwayyy()
     {
         this.animator.SetBool("Attack", false);
+        this.opponent = null;
     }
 
     private void OnTriggerEnter(Collider other)
     {
        if (other.gameObject.CompareTag("Turtle"))
-            LetsFight();
+        {
+
+            LetsFight(other.transform.parent.gameObject);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
