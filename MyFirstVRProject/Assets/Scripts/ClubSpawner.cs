e12bbc62fd76001b4561de22c7e02664a8da48be
NAIex <alexneagubiz@gmail.com>
2024-10-13 18:58:55 +0300

    Teleport club to user

---
+++ MyFirstVRProject/Assets/Scripts/ClubSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClubSpawner : MonoBehaviour
+{
+    public InputActionProperty createButton;
+    public GameObject clubPrefab;
+
+    private GameObject clubRef;
+    private void Awake()
+    {
+        clubRef = Instantiate(clubPrefab,gameObject.transform.position, gameObject.transform.rotation);
+        clubRef.SetActive(false);
+    }
+    private void TeleportClub()
+    {
+        clubRef.SetActive(false);
+
+        Rigidbody comp = clubRef.GetComponent<Rigidbody>();
+        comp.velocity = Vector3.zero;
+        comp.angularVelocity= Vector3.zero;
+
+        clubRef.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+
+        clubRef.SetActive(true);
+    }
+    private void Update()
+    {
+        if(createButton.action.WasPressedThisFrame())
+        {
+            TeleportClub();
+        }
+    }
+}
